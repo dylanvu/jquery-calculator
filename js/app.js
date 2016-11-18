691a7e698323fddfecd9e24bec1ce29c22d1097d
Dylan Vu <dylan.n.vu@gmail.com>
2016-11-18 02:43:49 -0800

    add event listener on equal

--- js/app.js
+++ js/app.js
@@ -2,17 +2,21 @@
   'use strict';
 
   const $screen = $('#screen');
+  $screen.text('0');
 
   $('#clear').on('click', () => {
-    $screen.text('');
+    $screen.text('0');
   });
 
   $('div.buttons').on('click', 'span:not("#clear"):not("#equals")', (event) => {
     const $target = $(event.target);
-    $('#screen').text($screen.text() + $target.text());
+    if ($screen.text() === '0')
+      $screen.text('');
+    $screen.text($screen.text() + $target.text());
   });
 
-  // $('#equals').on('click', () => {
-  //
-  // });
+  $('#equals').on('click', () => {
+    const result = eval($screen.text().replace(/x/g,'*').replace(/%/g,'/'));
+    $screen.text(result.toString());
+  });
 })();
